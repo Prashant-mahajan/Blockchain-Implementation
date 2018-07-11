6f13b7beaf625afa58f64e51cce306f2db4f30e3
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-07-11 21:34:20 +0530

    Implementing Proof of Consensus

--- Blockchain.py
+++ Blockchain.py
@@ -18,6 +18,21 @@ class Blockchain:
 		# Create the genesis block
 		self.new_block(previous_hash = 1, proof=100)
 
+		self.nodes = set()
+		# Set is used to ensure that no matter how many times we add a specific node, it appears exactly once
+
+
+	def register_node(self,address):
+		"""
+		Add a new node to the list of nodes
+
+		:param address: <str> Address of node. Eg. 'http://192.168.2.5:5000'
+		:return: None
+		"""
+
+		parsed_url = urlparse(address)
+		self.nodes.add(parsed_url.netloc)
+
 
 	def new_block(self, proof, previous_hash):
 		'''
@@ -107,6 +122,53 @@ class Blockchain:
 		guess = f'{last_proof}{proof}'.encode()
 		guess_hash = hashlib.sha256(guess).hexdigest()
 		return guess_hash[:4] == '0000'
+
+	def valid_chain(self, chain):
+		"""
+		Determine if a given blockchain is valid
+
+		:param chain: <list> A blockchain
+		:return: <bool> True if valid, False if not
+		"""
+
+		last_block = chain[0]
+		current_index = 1
+
+		while current_index < len(chain):
+			block = chain[current_index]
+			print(f'{last_block}')
+			print (f'{block}')
+			print("\n-------------\n")
+			# Check that the hash of the block is correct
+			if block['previous_hash'] != self.hash(last_block):
+				return False
+
+			# Check that the Proof of Work is correct
+			if not self.valid_proof(last_block['proof'], block['proof']):
+				return False
+
+			last_block = block
+			current_index += 1
+
+		return True
+
+	def resolve_conflicts(self):
+		"""
+		This is our Consensus Algorihm, it resolves conflicts
+		by replacing our chain with the longest one in the network.
+
+		:return: <bool> True if our chain was replaced, False if not
+		"""
+
+		neighbours = self.nodes
+		new_chain = None
+
+		# We're only looking for chains longer than ours
+		max_length = len(self.chain)
+
+		# Grab and verify the chains from all the nodes in our network
+		
+
 
 	""" ------------------------------- Setting up the Flask ------------------------------- """
 
