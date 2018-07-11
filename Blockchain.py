458c7799dd1b0194c39117cfd96fff2e4cd48329
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-07-11 22:02:51 +0530

    Proof of consensus implemented

--- Blockchain.py
+++ Blockchain.py
@@ -184,7 +184,7 @@ class Blockchain:
 		self.chain = new_chain
 		return True
 
-	return False 
+	return False
 
 
 	""" ------------------------------- Setting up the Flask ------------------------------- """
@@ -248,6 +248,43 @@ def full_chain():
 		'length': len(blockchain.chain),
 	}
 	return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+	values = request.get_json()
+
+	nodes = values.get('nodes')
+	if nodes is None:
+		return "Error: Please supply a valid list of nodes", 400
+
+	for node in nodes:
+		blockchain.register_node(node)
+
+	response = {
+		'message' : 'New nodes have been added',
+		'total_nodes' : list(blockchain.nodes),
+	}
+
+	return jsonify(response), 201
+
+
+@app.route('nodes/resolve', methods=['GET'])
+def consensus():
+	replaced = blockchain.resolve_conflicts()
+
+	if replaced:
+		response = {
+			'message' : 'Our chain was replaced',
+			'new_chain' : blockchain.chain
+		}
+	else:
+		response = {
+			'message' : 'Our chain is authorative',
+			'chain' : blockchain.chain
+		}
+
+	return jsonify(response), 200
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
