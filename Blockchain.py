6e8565cc34191fdfb0ee5910b4336da1a7edc3a0
Prashant-mahajan <prashantmahajan70@gmail.com>
2018-07-12 23:06:11 +0530

    Bugs resolved

--- Blockchain.py
+++ Blockchain.py
@@ -31,8 +31,43 @@ class Blockchain:
 		"""
 
 		parsed_url = urlparse(address)
-		self.nodes.add(parsed_url.netloc)
+		if parsed_url.netloc:
+			self.nodes.add(parsed_url.netloc)
+		elif parsed_url.path:
+			# Accepts an URL without scheme like '192.168.0.5:5000'
+			self.nodes.add(parsed_url.path)
+		else:
+			raise ValueError('Invalid URL')
 
+	def valid_chain(self, chain):
+		"""
+		Determine if a given blockchain is valid
+
+		:param chain: <list> A blockchain
+		:return: <bool> True if valid, False if not
+		"""
+
+		last_block = chain[0]
+		current_index = 1
+
+		while current_index < len(chain):
+			block = chain[current_index]
+			print(f'{last_block}')
+			print (f'{block}')
+			print("\n-------------\n")
+			# Check that the hash of the block is correct
+			last_block_hash = self.hash(last_block)
+			if block['previous_hash'] != last_block_hash:
+				return False
+
+			# Check that the Proof of Work is correct
+			if not self.valid_proof(last_block['proof'], block['proof'], last_block_hash):
+				return False
+
+			last_block = block
+			current_index += 1
+
+		return True
 
 	def new_block(self, proof, previous_hash):
 		'''
@@ -59,13 +94,13 @@ class Blockchain:
 
 	def new_transaction(self, sender, recipient, amount):
 		"""
-        Creates a new transaction to go into the next mined Block
+		Creates a new transaction to go into the next mined Block
 
-        :param sender: <str> Address of the Sender
-        :param recipient: <str> Address of the Recipient
-        :param amount: <int> Amount
-        :return: <int> The index of the Block that will hold this transaction
-        """
+		:param sender: <str> Address of the Sender
+		:param recipient: <str> Address of the Recipient
+		:param amount: <int> Amount
+		:return: <int> The index of the Block that will hold this transaction
+		"""
 		self.current_transactions.append({
 			'sender': sender,
 			'recipient': recipient,
@@ -102,14 +137,17 @@ class Blockchain:
 		:return: <int>
 		'''
 
+		last_proof = last_block['proof']
+		last_hash = self.hash(last_block)
+
 		proof = 0
-		while self.valid_proof(last_proof, proof) is False:
+		while self.valid_proof(last_proof, proof, last_hash) is False:
 			proof += 1
 
 		return proof
 
 	@staticmethod
-	def valid_proof(last_proof, proof):
+	def valid_proof(last_proof, proof, last_hash):
 		'''
 
 		Validates the Proof: Does hash(last_proof, proof) contains 4 leading zeros?
@@ -119,46 +157,16 @@ class Blockchain:
 		:return: <bool> True if correct False if not
 		'''
 
-		guess = f'{last_proof}{proof}'.encode()
+		guess = f'{last_proof}{proof}{last_hash}'.encode()
 		guess_hash = hashlib.sha256(guess).hexdigest()
 		return guess_hash[:4] == '0000'
 
-	def valid_chain(self, chain):
-		"""
-		Determine if a given blockchain is valid
-
-		:param chain: <list> A blockchain
-		:return: <bool> True if valid, False if not
-		"""
-
-		last_block = chain[0]
-		current_index = 1
-
-		while current_index < len(chain):
-			block = chain[current_index]
-			print(f'{last_block}')
-			print (f'{block}')
-			print("\n-------------\n")
-			# Check that the hash of the block is correct
-			if block['previous_hash'] != self.hash(last_block):
-				return False
-
-			# Check that the Proof of Work is correct
-			if not self.valid_proof(last_block['proof'], block['proof']):
-				return False
-
-			last_block = block
-			current_index += 1
-
-		return True
-
 	def resolve_conflicts(self):
 		"""
-		This is our Consensus Algorihm, it resolves conflicts
-		by replacing our chain with the longest one in the network.
-
-		:return: <bool> True if our chain was replaced, False if not
-		"""
+        This is our consensus algorithm, it resolves conflicts
+        by replacing our chain with the longest one in the network.
+        :return: True if our chain was replaced, False if not
+        """
 
 		neighbours = self.nodes
 		new_chain = None
@@ -170,7 +178,7 @@ class Blockchain:
 		for node in neighbours:
 			response = requests.get(f'http://{node}/chain')
 
-			if respose.status_code == 200:
+			if response.status_code == 200:
 				length = response.json()['length']
 				chain = response.json()['chain']
 
@@ -180,14 +188,14 @@ class Blockchain:
 					new_chain = chain
 
 		# Replace our chain if we discovered a new, valid chain longer than ours
-	if new_chain:
-		self.chain = new_chain
-		return True
+		if new_chain:
+			self.chain = new_chain
+			return True
 
-	return False 
+		return False
 
 
-	""" ------------------------------- Setting up the Flask ------------------------------- """
+""" ------------------------------- Setting up the Flask ------------------------------- """
 
 # Instantiate our Node
 app = Flask(__name__)
@@ -202,7 +210,6 @@ blockchain = Blockchain()
 def mine():
 	# We run the proof of work algorithm to get the next proof
 	last_block = blockchain.last_block
-	last_proof = last_block['proof']
 	proof = blockchain.proof_of_work(last_proof)
 
 	# We must receive a reward for finding the proof.
@@ -248,6 +255,43 @@ def full_chain():
 		'length': len(blockchain.chain),
 	}
 	return jsonify(response), 200
+
+@app.route('/nodes/register', methods=['POST'])
+def register_nodes():
+	values = request.get_json()
+
+	nodes = values.get('nodes')
+	if nodes is None:
+		return "Error: Please supply a valid list of nodes", 400
+
+	for node in nodes:
+		blockchain.register_node(node)
+
+	response = {
+		'message' : 'New nodes have been added',
+		'total_nodes' : list(blockchain.nodes),
+	}
+
+	return jsonify(response), 201
+
+
+@app.route('/nodes/resolve', methods=['GET'])
+def consensus():
+	replaced = blockchain.resolve_conflicts()
+
+	if replaced:
+		response = {
+			'message': 'Our chain was replaced',
+			'new_chain': blockchain.chain
+		}
+	else:
+		response = {
+			'message': 'Our chain is authoritative',
+			'chain': blockchain.chain
+		}
+
+	return jsonify(response), 200
+
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=5000)
